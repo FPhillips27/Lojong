c203ad4d94803fa3a7cf4d222996713f96c91951
Forrest <mrforrestphillips@gmail.com>
2016-06-05 22:42:58 +0000

    Finished implementing display of a single, randomly selected entry.

--- app/models/lojong_saying.rb
+++ app/models/lojong_saying.rb
@@ -1,4 +1,4 @@
 class LojongSaying < ActiveRecord::Base
     scope :recent, lambda { order('created_at DESC').limit(10) }
-    scope :randomSaying, lambda { order('id DESC').shuffle }
+    scope :randomSaying, lambda { order('id DESC').sample(1).shuffle }
 end
