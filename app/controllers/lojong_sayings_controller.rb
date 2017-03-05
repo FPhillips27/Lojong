423240068af0af78320f6b9c05af9a49abcec57f
Forrest <mrforrestphillips@gmail.com>
2017-03-05 02:41:15 +0000

    Removing unused code

--- app/controllers/lojong_sayings_controller.rb
+++ app/controllers/lojong_sayings_controller.rb
@@ -5,40 +5,20 @@ class LojongSayingsController < ApplicationController
     end
         #GET
     def show
-        @lojong_saying = LojongSaying.find(params[:id])
     end
         #GET
     def new
-        @lojong_saying = LojongSaying.new
     end
         #GET
     def edit
-        @lojong_saying = LojongSaying.find(params[:id])
     end
         #POST
     def create
-        @lojong_saying = LojongSaying.new(content: params[:lojong_saying][:content], title: params[:lojong_saying][:number])
-        if @lojong_saying.save
-            redirect_to lojong_sayings_path, :notice => "Your Text Post has been saved."
-        else
-            @errors = @lojong_saying.errors
-            render :new
-        end
     end
         #PUT
     def update
-        @lojong_saying = LojongSaying.find(params[:id])
-        if @lojong_saying.update(content: params[:lojong_saying][:content], title: params[:lojong_saying][:number])
-            redirect_to lojong_sayings_path, :notice => "Your Text Post has been saved."
-        else
-            @errors = @lojong_saying.errors
-            render :edit
-        end
     end
         #DELETE
     def destroy
-        @lojong_saying = LojongSaying.find(params[:id])
-        @lojong_saying.destroy
-        redirect_to lojong_sayings_path,  :notice => "Your Text Post has been deleted"
     end
 end
